fc9aeb418164eb4016817ff0dc6da4c118ebb8de
Ricael <ricaelmenezes@gmail.com>
2025-08-13 10:15:04 -0300

    add: first commit - adding the project on GitHub

---
+++ src/main/java/br/com/aura_bank/auraBank/AuraBankApplication.java
@@ -0,0 +1,13 @@
+package br.com.aura_bank.auraBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuraBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuraBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/aura_bank/auraBank/AuraBankApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aura_bank.auraBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuraBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
